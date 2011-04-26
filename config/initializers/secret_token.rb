d62af59e5ef053c442b18cd129c61c2756f1a5c7
taztwister <todd.silvia@gmail.com>
2011-04-25 22:21:28 -0500

    Initial Ruby Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cs441Proj4::Application.config.secret_token = '9006d61f1e9e61a1ca06ae335d2a86b85365db2e83d29ba622baa8c36249d9e2caf82c35c0b707ffd484fdc9b4cfab592a29eb0ee02bbf07726825adc86b3359'
